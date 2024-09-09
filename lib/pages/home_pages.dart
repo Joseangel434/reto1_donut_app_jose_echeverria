0c45c2ceef878ac8f1b3d608b8d27c604cbadfac
jose <joseecheverria573@gmail.com>
2024-09-09 15:39:53 -0600

    Tabbar_añadido

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:reto1_donut_app_jose_echeverria/tab/burger_tab.dart';
+import 'package:reto1_donut_app_jose_echeverria/tab/donut_tab.dart';
+import 'package:reto1_donut_app_jose_echeverria/tab/panCake_tab.dart';
+import 'package:reto1_donut_app_jose_echeverria/tab/pizza_tab.dart';
+import 'package:reto1_donut_app_jose_echeverria/tab/smoothie_tab.dart';
+import 'package:reto1_donut_app_jose_echeverria/utils/my_tab.dart';
 
 class HomePages extends StatefulWidget {
   const HomePages({super.key});
@@ -8,52 +14,82 @@ class HomePages extends StatefulWidget {
 }
 
 class _HomePagesState extends State<HomePages> {
+
+  List <Widget> mytabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png',),
+    //burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png',),
+    // smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png',),
+    // panCake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png',),
+    // pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png',),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 24.0),
-          child: IconButton(
-            icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
-            onPressed: () {
-              print("hola mundo");
-            },
+    return DefaultTabController(
+      length: mytabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
+              onPressed: () {
+                print("hola mundo");
+              },
+            ),
           ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(
+                Icons.person,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+            ),
+          ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Icon(
-              Icons.person,
-              color: Colors.grey[800],
-              size: 36,
+        body: Column(
+          children: [
+            //Texto I want to eat
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text(
+                    "I want to ",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text("Eat",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline)),
+                ],
+              ),
             ),
-          ),
-        ],
+            TabBar(tabs: mytabs),
+
+           const Expanded(child: TabBarView(children:[
+              //donut
+              DonutTab(),
+              //burger
+              BurgerTab(),
+              //smoothie
+              SmoothieTab(),
+              //panCake
+              PancakeTab(),
+              //pizza
+              PizzaTab(),
+            ]))
+          ],
+       ),
       ),
-      body: const Column(
-        children: [
-          //Texto I want to eat
-          Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Row(
-              children: [
-                Text(
-                  "I want to ",
-                  style: TextStyle(fontSize: 32),
-                ),
-                Text("Eat",
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline)),
-              ],
-            ),
-          )
-        ],
-     ),
-);
+    );
 }
 }
